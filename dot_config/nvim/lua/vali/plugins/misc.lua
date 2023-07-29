d726d2dbb6f04a88e79e58745a76ab1e05116ff2
Vali <vali@tictocgames.com>
2023-07-29 22:17:09 +0800

    update

--- dot_config/nvim/lua/vali/plugins/misc.lua
+++ dot_config/nvim/lua/vali/plugins/misc.lua
@@ -36,4 +36,16 @@ return {
             { "<leader>u", vim.cmd.UndoTreeToggle, desc = 'Undo Tree' }
         }
     },
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre",
+        opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+        -- stylua: ignore
+        keys = {
+            { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
+            { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+            { "<leader>qd", function() require("persistence").stop() end,                desc =
+            "Don't Save Current Session" },
+        },
+    }
 }
