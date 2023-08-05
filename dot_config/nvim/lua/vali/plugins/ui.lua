fdb6b9e9baecded23cca6b6ceb3cab6af95ada67
Daniel Vallesteros <vali@Daniels-MacBook-Pro.local>
2023-08-05 19:11:22 +0800

    disable progress dispaly from noice

--- dot_config/nvim/lua/vali/plugins/ui.lua
+++ dot_config/nvim/lua/vali/plugins/ui.lua
@@ -201,8 +201,10 @@ return {
         "folke/noice.nvim",
         event = "VeryLazy",
         opts = {
-
             lsp = {
+                progress = {
+                    enabled = false
+                },
                 -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
                 override = {
                     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
