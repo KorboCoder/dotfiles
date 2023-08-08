bd39d9f93467fa0727c4169b1010731e2dd1af47
Vali <vali@tictocgames.com>
2023-08-08 22:24:13 +0800

    added notif for test start and results

--- dot_config/nvim/lua/vali/plugins/test.lua
+++ dot_config/nvim/lua/vali/plugins/test.lua
@@ -20,6 +20,47 @@ return {
                         end,
                     }),
                 },
+                consumers = {
+                    -- notify when tests start and end
+                    notify = function(client)
+                        client.listeners.started = function()
+                                require("neotest.lib").notify("Starting Tests...")
+                        end
+                        client.listeners.results = function(_, results, partial)
+
+                            if partial then
+                                return
+                            end
+                            local firstResult = nil
+                            local resTable = {
+                                passed = 0,
+                                failed = 0,
+                                skipped = 0
+
+                            }
+                            -- this is not accurate to actual test results, might need to configure for each test adapter
+                            for _, value in pairs(results) do
+                                resTable[value.status] = resTable[value.status] + 1;
+                                if(value.status == 'passed' or value.status == 'failed') then
+                                    firstResult = value
+                                end
+                            end
+
+                            if resTable.passed + resTable.failed > 1 then
+                                require("neotest.lib").notify("Tests completed:\nPassed: " ..
+                                resTable.passed .. "\nFailed: " .. resTable.failed .. "\nSkipped: " .. resTable.skipped)
+                            elseif firstResult ~= nil then
+                                local level = vim.log.levels.INFO
+                                if firstResult.status == "failed" then
+                                    level = vim.log.levels.ERROR
+                                end
+
+                                require("neotest.lib").notify("Test Result: " .. firstResult.status, level)
+                            end
+                        end
+                        return {}
+                    end,
+                },
                 output = {
                     open_on_run = true
                 },
