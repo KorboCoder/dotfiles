0811c8da32624781348721bb9186760f5b3f2e5e
Daniel Vallesteros <danielvallesteros@daniels-mbp.home>
2024-06-21 23:21:51 -0400

    update

--- dot_config/nvim/lua/vali/plugins/lsp.lua
+++ dot_config/nvim/lua/vali/plugins/lsp.lua
@@ -121,7 +121,40 @@ return {
             --  Reference: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
             local servers = {
                 -- clangd = {},
-                -- gopls = {},
+                gopls = {
+                    gofumpt = true,
+                    codelenses = {
+                        gc_details = false,
+                        generate = true,
+                        regenerate_cgo = true,
+                        run_govulncheck = true,
+                        test = true,
+                        tidy = true,
+                        upgrade_dependency = true,
+                        vendor = true,
+                    },
+                    hints = {
+                        assignVariableTypes = true,
+                        compositeLiteralFields = true,
+                        compositeLiteralTypes = true,
+                        constantValues = true,
+                        functionTypeParameters = true,
+                        parameterNames = true,
+                        rangeVariableTypes = true,
+                    },
+                    analyses = {
+                        fieldalignment = true,
+                        nilness = true,
+                        unusedparams = true,
+                        unusedwrite = true,
+                        useany = true,
+                    },
+                    usePlaceholders = true,
+                    completeUnimported = true,
+                    staticcheck = true,
+                    directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+                    semanticTokens = true,
+                },
                 -- pyright = {},
                 -- rust_analyzer = {},
                 -- cql = {},
@@ -368,7 +401,6 @@ return {
     },
     {
         "MysticalDevil/inlay-hints.nvim",
-        enabled = false,
         event = "LspAttach",
         dependencies = { "neovim/nvim-lspconfig" },
         keys = {
