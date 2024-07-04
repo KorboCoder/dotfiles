3a8b9fe8ae015a24ef01c91c577421a3a3ca8fc7
Daniel Vallesteros <dmvallesteros@gmail.com>
2024-07-04 13:35:46 -0400

    added neorg.lua

---
+++ dot_config/nvim/lua/vali/plugins/neorg.lua
@@ -0,0 +1,22 @@
+return {
+    -- NOTE: install treesitter wthn  `CC=gcc-14 nvim -c "TSInstall norg"`
+    {
+        "nvim-neorg/neorg",
+        -- lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        version = "*", -- Pin Neorg to the latest stable release
+        opts = {
+            load = {
+                ["core.defaults"] = {},
+                ["core.concealer"] = {},
+                ["core.dirman"] = {
+                    config = {
+                        workspaces = {
+                            notes = "~/notes",
+                        },
+                        default_workspace = "notes",
+                    },
+                },
+            },
+        }
+    }
+}
